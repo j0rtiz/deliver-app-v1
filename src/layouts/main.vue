4725fbd797014f6175d1aa0514a4120a5dfd6222
j0rtiz <j0rtiz@protonmail.ch>
2021-03-29 10:28:27 -0300

    refactor(main): Título e descrição

--- src/layouts/main.vue
+++ src/layouts/main.vue
@@ -2,7 +2,11 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-toolbar-title> deliver-app-v1 </q-toolbar-title>
+        <q-toolbar-title>
+          <div class="q-pt-xs">deliver-app-v1</div>
+
+          <div class="text-caption q-pb-sm">Teste prático Deliver App versão 1</div>
+        </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
