842e2b8f7b21b127b6a489235b1db7f314d76bec
j0rtiz <j0rtiz@protonmail.ch>
2021-03-29 01:45:37 -0300

    fix(projeto): Heroku build

---
+++ server.js
@@ -0,0 +1,10 @@
+const history = require('connect-history-api-fallback');
+const serveStatic = require('serve-static');
+const express = require('express');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(history());
+app.use(serveStatic(__dirname + '/dist/spa'));
+app.listen(port);
